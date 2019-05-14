bd90e3c09c90183c309adf325b718942edafab24
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-05-14 23:22:45 +0100

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#247BA0`,
+        theme_color: `#247BA0`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dydx.png`, // This path is relative to the root of the site.
       },
     },
     {
